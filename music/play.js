8935b2ac4c256b3b05f0ad9a989da3b34d556361
andrew-noah <andrewnoah.relay@gmail.com>
2022-07-21 23:39:14 -0700

    Fixed the queue breaking when an invalid link was requested

--- music/play.js
+++ music/play.js
@@ -1,5 +1,5 @@
-const { MessageEmbed } = require('discord.js');
-
+const { Message, MessageEmbed } = require('discord.js');
+const { getVoiceConnection } = require('@discordjs/voice');
 const ytSearch = require('yt-search');
 
 
@@ -17,46 +17,52 @@ exports.queue = queue;
 
 const ytPlay = require('play-dl');
 
-play = async (connection, search, channel) => {
+/**
+ * @param {Message} msg
+ */
+play = async (msg, search, channel) => {
 
     const searchResults = await ytSearch(search);
     if(!searchResults.videos[0]) {
         msg.channel.send('Couldn\'t find a matching video');
-        play(connection, queue[0], channel);
-    }
-
-    const song = searchResults.videos[0];
-
-    const { stream } = await ytPlay.stream(song.url, { discordPlayerCompatibility: true });
-
-    //TODO: Figure out how to make streaming work side by side
-    //const stream = ytdl(song.url, {highWaterMark: 1<<25, quality: [91,92,93,94,95], liveBuffer: 4900});
-
-    const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-    const player = createAudioPlayer();
-
-    player.play(resource)
-    connection.subscribe(player);
-    
-    // With a full youtube URL we can extract the code at the end
-    // and grab a thumbnail for the embeded portion
-    let ytCode = song.url.split('v=').pop();
-
-    // Create an embedded message showing information about the video
-    const embed = new MessageEmbed()
-        .setTitle(`Now Playing: ${song.title}`)
-        .setDescription(`${song.author.name} - ${song.timestamp}`)
-        .setURL(`${song.url}`)
-        .setThumbnail(`https://img.youtube.com/vi/${ytCode}/0.jpg`);
-
-    channel.send({ embeds: [embed] });
-
-    // Figure out what to do next when the current video ends
-    player.on(AudioPlayerStatus.Idle, () => {
         queue.shift();
-        if(!queue.length) connection.destroy();
-        else play(connection, queue[0], channel);
-    });
+        if(!queue.length) return;
+        play(msg, queue[0], channel);
+    }
+    else {
+        const song = searchResults.videos[0];
+        const connection = getVoiceConnection(msg.guild.id);
+        const { stream } = await ytPlay.stream(song.url, { discordPlayerCompatibility: true });
+
+        //TODO: Figure out how to make streaming work side by side
+        //const stream = ytdl(song.url, {highWaterMark: 1<<25, quality: [91,92,93,94,95], liveBuffer: 4900});
+
+        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+        const player = createAudioPlayer();
+
+        player.play(resource)
+        connection.subscribe(player);
+        
+        // With a full youtube URL we can extract the code at the end
+        // and grab a thumbnail for the embeded portion
+        let ytCode = song.url.split('v=').pop();
+
+        // Create an embedded message showing information about the video
+        const embed = new MessageEmbed()
+            .setTitle(`Now Playing: ${song.title}`)
+            .setDescription(`${song.author.name} - ${song.timestamp}`)
+            .setURL(`${song.url}`)
+            .setThumbnail(`https://img.youtube.com/vi/${ytCode}/0.jpg`);
+
+        channel.send({ embeds: [embed] });
+
+        // Figure out what to do next when the current video ends
+        player.on(AudioPlayerStatus.Idle, () => {
+            queue.shift();
+            if(!queue.length) connection.destroy();
+            else play(msg, queue[0], channel);
+        });
+    }
 }
 
 exports.play = play;
